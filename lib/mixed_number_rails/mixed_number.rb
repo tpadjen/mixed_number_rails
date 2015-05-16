88549215530d9e0cdcf946f94dc2b2e7c3f280b6
Tim Padjen <tpadjen@gmail.com>
2015-05-16 12:35:03 -0600

    Separate MixedNumber

---
+++ lib/mixed_number_rails/mixed_number.rb
@@ -0,0 +1,20 @@
+require "mixed_number"
+
+class MixedNumber
+
+	class << self
+		def load(decimal)
+			self.parse(decimal)
+		end
+
+		def dump(obj)
+			unless obj.is_a?(self) || obj.respond_to?(:to_m)
+	      raise ::ActiveRecord::SerializationTypeMismatch,
+	        "Attribute was supposed to be a #{self}, but was a #{obj.class}. -- #{obj.inspect}"
+	    end
+
+	    obj.to_m.to_d
+		end
+	end
+
+end
